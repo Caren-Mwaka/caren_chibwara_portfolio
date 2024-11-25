624ee13b3d7838fd0cb355141432391a352ff888
Caren Wanjiru Mwaka Chibwara <carrienjiru40@gmail.com>
2024-11-25 16:32:25 +0300

    added spacing to the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,26 @@
-
-import './App.css'
-import About from './components/About'
-import Contact from './components/Contact'
-import Experience from './components/Experience'
-import Home from './components/Home'
-import NavBar from './components/NavBar'
-import Portfolio from './components/Portfolio'
-import SocialLinks from './components/SocialLinks'
+import "./App.css";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Experience from "./components/Experience";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Portfolio from "./components/Portfolio";
+import SocialLinks from "./components/SocialLinks";
 
 function App() {
-  
-
   return (
     <>
-      <div>
-        
-      <NavBar/>
-      <Home/>
-      <About/>
-      <Portfolio/>
-      <Experience/>
-      <Contact/>
-      <SocialLinks/>
-      
+      <div className="space-y-10">
+        <NavBar />
+        <Home />
+        <About />
+        <Portfolio />
+        <Experience />
+        <Contact />
+        <SocialLinks />
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
